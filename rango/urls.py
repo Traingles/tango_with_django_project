bd68686eb0b0236cda995b9515fa288c3a0f0f19
Nathan Kirkpatrick <2283945k@student.gla.ac.uk>
2019-02-07 21:28:16 +0000

    Chapter 6 Fixed

--- rango/urls.py
+++ rango/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     url(r'^about/$', views.about, name='about'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
         views.show_category, name='show_category'),
-    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',
-        views.add_page, name='add_page'),
 ]
